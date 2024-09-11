bb4cad99bc53a4b14b385189c7788223e5ecd9a4
Rainot <olga24@onet.pl>
2024-09-11 16:11:55 +0200

    Adding everything

---
+++ Project/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private float horizontal;
+    private float vertical;
+    private float speed = 4.0f;
+    Rigidbody2D rb;
+    
+    private int health = 100;
+
+    public bool turnedRight = false;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal= Input.GetAxisRaw("Horizontal");
+        vertical= Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector2(horizontal*speed, vertical*speed);
+        turnedRight = false;
+        if(horizontal > 0)
+        {
+            GetComponent<Animator>().Play("Right");
+            turnedRight = true;
+        } else if (horizontal < 0)
+        {
+            GetComponent<Animator>().Play("Left");
+        } else if (vertical > 0)
+        {
+            GetComponent<Animator>().Play("Up");
+        } else if (vertical < 0)
+        {
+            GetComponent<Animator>().Play("Down");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+            Invoke("HidePlayerBlood", 0.25f);
+            
+        }
+    }
+
+    void HidePlayerBlood()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
+    }
+    
+}
